848c077dc374ff86e375c9d0831a2b9dd960c8b9
revanthyenugudhati <sivajirevanth.1@gmail.com>
2018-02-18 14:25:31 +0530

    This is second commit.

--- app/src/main/java/com/example/revanthsivaji/vvit_canteen/ListActivity.java
+++ app/src/main/java/com/example/revanthsivaji/vvit_canteen/ListActivity.java
@@ -13,13 +13,15 @@ public class ListActivity extends AppCompatActivity {
     Button rspb,rsmb,edspb,edsmb,fipb,fimb;
     TextView spq,spp,tuq,tup,fq,fp,sq,sp,dmq,dmp,dmsq,dmsp,kkq,kkp,fsq,fsp,boq,bop,ppq,ppp;
     TextView dfq,dfp,hsq,hsp,mmq,mmp,ubq,ubp,bq,bp,lp,lq,pq,pp,saq,sap,tp,tq,rsq,rsp,edsq,edsp,fip,fiq;
+    int values=0,valuetu=0,valuef=0,valuesd=0,valrs=0,valeds=0,valfi=0,valp=0,valsa=0,valt=0;
+    int sprite_p=0,tu_p=0,f_p=0,sd_p=0,rs_p=0,eds_p=0,fi_p=0,p_p=0,sa_p=0,t_p=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         spmb=(Button)findViewById(R.id.spritemb);
-        sppb=(Button)findViewById(R.id.spritemb);
+        sppb=(Button)findViewById(R.id.spritepb);
         tumb=(Button)findViewById(R.id.thumpsmb);
         tupb=(Button)findViewById(R.id.thumpspb);
         fmb=(Button)findViewById(R.id.fantamb);
@@ -63,7 +65,7 @@ public class ListActivity extends AppCompatActivity {
         fimb=(Button)findViewById(R.id.filemb);
         fipb=(Button)findViewById(R.id.filepb);
         spq=(TextView)findViewById(R.id.spriteq);
-        spp=(TextView)findViewById(R.id.spriteq);
+        spp=(TextView)findViewById(R.id.spritep);
         tuq=(TextView)findViewById(R.id.thumpsq);
         tup=(TextView)findViewById(R.id.thumpsp);
         fq=(TextView)findViewById(R.id.fantaq);
@@ -106,13 +108,258 @@ public class ListActivity extends AppCompatActivity {
         edsp=(TextView)findViewById(R.id.edsp);
         fiq=(TextView)findViewById(R.id.fileq);
         fip=(TextView)findViewById(R.id.filep);
+        sppb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values=values+1;
+                sprite_p=sprite_p+20;
+                spq.setText(String.valueOf(values));
+                spp.setText(String.valueOf(sprite_p));            }
+        });
+        spmb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                values=values-1;
+                sprite_p=sprite_p-20;
+                if(values<0)
+                {
+                    values=0;
+                }
+                if(sprite_p<0)
+                {
+                    sprite_p=0;
+                }
+                spq.setText(String.valueOf(values));
+                spp.setText(String.valueOf(sprite_p));
+            }
+        });
+        tupb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuetu=valuetu+1;
+                tu_p=tu_p+20;
+                tuq.setText(String.valueOf(valuetu));
+                tup.setText(String.valueOf(tu_p));            }
+        });
+        tumb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuetu=valuetu-1;
+                tu_p=tu_p-20;
+                if(valuetu<0)
+                {
+                    valuetu=0;
+                }
+                if(tu_p<0)
+                {
+                    tu_p=0;
+                }
+                tuq.setText(String.valueOf(valuetu));
+                tup.setText(String.valueOf(tu_p));
+            }
+        });
+        fpb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuef=valuef+1;
+                f_p=f_p+20;
+                fq.setText(String.valueOf(valuef));
+                fp.setText(String.valueOf(f_p));            }
+        });
+        fmb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuef=valuef-1;
+                f_p=f_p-20;
+                if(valuef<0)
+                {
+                    valuef=0;
+                }
+                if(f_p<0)
+                {
+                    f_p=0;
+                }
+                fq.setText(String.valueOf(valuef));
+                fp.setText(String.valueOf(f_p));
+            }
+        });
+        spb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuesd=valuesd+1;
+                sd_p=sd_p+20;
+                sq.setText(String.valueOf(valuesd));
+                sp.setText(String.valueOf(sd_p));            }
+        });
+        smb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuesd=valuesd-1;
+                sd_p=sd_p-20;
+                if(valuesd<0)
+                {
+                    valuesd=0;
+                }
+                if(sd_p<0)
+                {
+                    sd_p=0;
+                }
+                sq.setText(String.valueOf(valuesd));
+                sp.setText(String.valueOf(sd_p));
+            }
+        });
+        ppb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valp=valp+1;
+                p_p=p_p+15;
+                pq.setText(String.valueOf(valp));
+                pp.setText(String.valueOf(p_p));            }
+        });
+        pmb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valp=valp-1;
+                p_p=p_p-15;
+                if(valp<0)
+                {
+                    valp=0;
+                }
+                if(p_p<0)
+                {
+                    p_p=0;
+                }
+                pq.setText(String.valueOf(valp));
+                pp.setText(String.valueOf(p_p));
+            }
+        });
+        sapb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valsa=valsa+1;
+                sa_p=sa_p+10;
+                saq.setText(String.valueOf(valsa));
+                sap.setText(String.valueOf(sa_p));            }
+        });
+        samb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valsa=valsa-1;
+                sa_p=sa_p-10;
+                if(valsa<0)
+                {
+                    valsa=0;
+                }
+                if(sa_p<0)
+                {
+                    sa_p=0;
+                }
+                saq.setText(String.valueOf(valsa));
+                sap.setText(String.valueOf(sa_p));
+            }
+        });
+        tpb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valt=valt+1;
+                t_p=t_p+20;
+                tq.setText(String.valueOf(valt));
+                tp.setText(String.valueOf(t_p));            }
+        });
+        tmb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valt=valt-1;
+                t_p=t_p-20;
+                if(valuesd<0)
+                {
+                    valuesd=0;
+                }
+                if(sd_p<0)
+                {
+                    sd_p=0;
+                }
+                sq.setText(String.valueOf(valuesd));
+                sp.setText(String.valueOf(sd_p));
+            }
+        });
+        spb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuesd=valuesd+1;
+                sd_p=sd_p+20;
+                sq.setText(String.valueOf(valuesd));
+                sp.setText(String.valueOf(sd_p));            }
+        });
+        smb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuesd=valuesd-1;
+                sd_p=sd_p-20;
+                if(valuesd<0)
+                {
+                    valuesd=0;
+                }
+                if(sd_p<0)
+                {
+                    sd_p=0;
+                }
+                sq.setText(String.valueOf(valuesd));
+                sp.setText(String.valueOf(sd_p));
+            }
+        });
+        spb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuesd=valuesd+1;
+                sd_p=sd_p+20;
+                sq.setText(String.valueOf(valuesd));
+                sp.setText(String.valueOf(sd_p));            }
+        });
+        smb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuesd=valuesd-1;
+                sd_p=sd_p-20;
+                if(valuesd<0)
+                {
+                    valuesd=0;
+                }
+                if(sd_p<0)
+                {
+                    sd_p=0;
+                }
+                sq.setText(String.valueOf(valuesd));
+                sp.setText(String.valueOf(sd_p));
+            }
+        });
+        spb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuesd=valuesd+1;
+                sd_p=sd_p+20;
+                sq.setText(String.valueOf(valuesd));
+                sp.setText(String.valueOf(sd_p));            }
+        });
+        smb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuesd=valuesd-1;
+                sd_p=sd_p-20;
+                if(valuesd<0)
+                {
+                    valuesd=0;
+                }
+                if(sd_p<0)
+                {
+                    sd_p=0;
+                }
+                sq.setText(String.valueOf(valuesd));
+                sp.setText(String.valueOf(sd_p));
+            }
+        });
+
 
     }
 
-
-    public void spritemb(View view) {
-    }
-
-    public void spritepb(View view) {
-    }
 }
